5f26b0934112f1bba76d8007d0f354d0a82f542b
Adham <eelsharkawya@gmail.com>
2022-07-24 21:39:26 +0200

    todo task first commit

---
+++ lib/Presentation/todoTask2/todo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:task_1/Presentation/todoTask2/pages/home.dart';
+
+class ToDo extends StatelessWidget {
+  const ToDo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
---
+++ lib/core/utils/constant.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+const String onBoardingRoute = '/';
+const String loginScreenRoute = '/login';
+const String signUpScreenRoute = '/signUp';
+const String homeScreenRoute = '/home';
+// const String homeScreenRoute = '/home';
+// const String homeScreenRoute = '/home';
+// const String homeScreenRoute = '/home';
+// const String homeScreenRoute = '/home';
+
+const Color kMainColor = Color(0XFFF98903);
